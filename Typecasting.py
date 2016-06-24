34822e0ec67361dcaf9a7a1e5c7fefaa211df932
Andrew <provoke111@gmail.com>
2016-06-24 21:19:36 +1000

    Practicing Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,10 @@
+name = "Bill"
+count = 1234
+floaty = 95.9500
+stranger = str(count)
+intfloat = float(count)
+print("Integer: ", count)
+print("String: ", name)
+print("Float: ", floaty)
+print("Integer to String: ", stranger)
+print("Integer to Float: ", intfloat)
